c55e0b0fa1ca9a22ed65e2b35a7ca544586adadc
gsrathoreniks <niks.gajsa@gmail.com>
2020-10-28 16:11:40 +0530

    binding instance created & set to root.

--- app/src/main/java/in/gsrathoreniks/viewbinding/MainActivity.kt
+++ app/src/main/java/in/gsrathoreniks/viewbinding/MainActivity.kt
@@ -1,11 +1,27 @@
 package `in`.gsrathoreniks.viewbinding
 
+import `in`.gsrathoreniks.viewbinding.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+/*
+    Declaring binding variable of type ActivityMainBinding which
+    contain reference to all the fields of activity_main.xml
+*/
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+//        Creates an instance of binding class for the activity to use
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+//        Get reference to the root of the view
+        val view = binding.root
+
+//        passing the root to setContentView()
+        setContentView(view)
     }
 }
